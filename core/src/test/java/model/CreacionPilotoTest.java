798b664e3b31c48ff518a65930b373c46762e291
Cordoba-Javier <ncordoba@alumnos.undec.edu.ar>
2024-10-19 15:56:27 -0300

    test creados no probados }

---
+++ core/src/test/java/model/CreacionPilotoTest.java
@@ -0,0 +1,14 @@
+package model;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+@ExtendWith(MockitoExtension.class)
+public class CrearPiloto {
+    @Mock
+    private 
+    @Test
+
+}
